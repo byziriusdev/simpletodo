05ed87ca10ed3044f86d54ad101c5249122b2a0f
byziriusdev <ziriusdevelopment@gmail.com>
2020-05-08 01:55:02 +0200

    Added style.css with the task-checked class that puts a line through the label. Modify index.html to add this css file. Added clear tasks board functionality to app.js. Added delete and check single task functionality to app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ loadEvents();
 function loadEvents() {
     // groups all events the app is gonna listen to
     document.querySelector('form').addEventListener('submit', submit);
+    document.getElementById('clear').addEventListener('click', clearTasksboard);
+    document.querySelector('ul').addEventListener('click', deleteOrTick);
 }
 
 function submit(e) {
@@ -22,4 +24,32 @@ function addTask(task) {
     li.innerHTML = `<span class="delete">Del</span><input type="checkbox"><label>${task}</label>`;
 
     ul.appendChild(li);
+}
+
+function clearTasksboard(e) {
+    let ul = document.querySelector('ul').innerHTML = '';
+}
+
+function deleteOrTick(e) {
+    if(e.target.className == 'delete') deleteTask(e);
+    else { tickTask(e) }
+}
+
+function deleteTask(e) {
+    // parent node of a .delete element is a li
+    let task = e.target.parentNode;
+    // parent node of the task (li element) is ul
+    let taskList = task.parentNode;
+    taskList.removeChild(task);
+}
+
+function tickTask(e) {
+    // next sibling of checkbox is the task label
+    const taskLabel = e.target.nextSibling;
+    if(e.target.checked) {
+        taskLabel.classList.add('task-checked');
+    } else {
+        taskLabel.classList.remove('task-checked');
+    }
+
 }
